3cd653308f4b76ab0ee600e735779cbbf9c1d57c
Sumeet <sumeet@fitmonk.co.in>
2016-05-19 12:42:15 +0530

    Updated license

--- src/main/java/com/CustomDriverManager.java
+++ src/main/java/com/CustomDriverManager.java
@@ -1,27 +1,3 @@
-/*
- * Copyright (c) 1996, 2011, Oracle and/or its affiliates. All rights reserved.
- * ORACLE PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- */
 
 package com;
 
