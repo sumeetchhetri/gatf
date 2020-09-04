3266edf12cba8e4fdcd346916e7ed54cd3a36046
Sumeet Chhetri <sumeet.chhetri@gmail.com>
2020-09-04 19:23:35 +0530

    Fixed issue with local folder mapping.

--- ci-test-bin-vnc.sh
+++ ci-test-bin-vnc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker run --rm -d -p 9080:9080 -p 4444:4444 -p 5900:5900 -v ./sample:/workdir -i sumeetchhetri/gatf-bin-vnc
+docker run --rm -d -p 9080:9080 -p 4444:4444 -p 5900:5900 -v sample:/workdir -i sumeetchhetri/gatf-bin-vnc
 echo "Waiting for gatf to listen on port 9080..."
 COUNTER=0
 while ! nc -z localhost 9080; do   
--- ci-test-bin.sh
+++ ci-test-bin.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker run --rm -d -p 9080:9080 -p 4444:4444 -v ./sample:/workdir -i sumeetchhetri/gatf-bin
+docker run --rm -d -p 9080:9080 -p 4444:4444 -v sample:/workdir -i sumeetchhetri/gatf-bin
 echo "Waiting for gatf to listen on port 9080..."
 COUNTER=0
 while ! nc -z localhost 9080; do   
