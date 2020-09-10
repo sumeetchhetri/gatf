85c894a5aec024c905402375efd7adb58c6dac71
Sumeet Chhetri <sumeet.chhetri@gmail.com>
2020-09-10 20:41:30 +0530

    Fixed issue with supervisor gatf start

--- artifacts/run.sh
+++ artifacts/run.sh
@@ -4,4 +4,4 @@ export GEOMETRY="${SCREEN_WIDTH}""x""${SCREEN_HEIGHT}""x""${SCREEN_DEPTH}"
 rm -f /tmp/.X*lock
 /usr/bin/Xvfb ${DISPLAY} -screen 0 ${GEOMETRY} -dpi ${SCREEN_DPI} -ac +extension RANDR &
 
-java -DD_DOCKER=true -DD_JAVA_HOME=${D_JAVA_HOME} -DD_CHROME_DRIVER=${D_CHROME_DRIVER} -DD_GATF_JAR=${D_GATF_JAR} -jar /gatf-alldep.jar "$@" > gatf.log 2>&1
+java -DD_DOCKER=true -DD_JAVA_HOME=${D_JAVA_HOME} -DD_CHROME_DRIVER=${D_CHROME_DRIVER} -DD_GATF_JAR=${D_GATF_JAR} -jar /gatf-alldep.jar "$@" > /workdir/gatf.log 2>&1
