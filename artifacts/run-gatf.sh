dab8bafd78d814f01d671d7e97e34a2afe62f0fc
Sumeet Chhetri <sumeet.chhetri@gmail.com>
2020-09-10 17:16:17 +0530

    Fixed issues with docker builds

--- artifacts/run-gatf.sh
+++ artifacts/run-gatf.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -jar /gatf-alldep.jar -configtool 9080 0.0.0.0 /workdir > gatf.log 2>&1
+java -DD_JAVA_HOME=${D_JAVA_HOME} -DD_CHROME_DRIVER=${D_CHROME_DRIVER} -DD_GATF_JAR=${D_GATF_JAR} -jar /gatf-alldep.jar -configtool 9080 0.0.0.0 /workdir > gatf.log 2>&1
